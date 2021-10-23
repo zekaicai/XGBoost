87791a113ecd625bd2dbceed31eb64fb7aa5f4a3
caizekai <caizekai@bytedance.com>
2021-10-23 17:16:45 +0800

    lr 不作用于第一棵树和最后一棵树

--- XGBoostRegressor.py
+++ XGBoostRegressor.py
@@ -11,7 +11,7 @@ def amse(y, y_hat):
 class XGBoostRegressor:
     def __init__(self, num_iters, num_bins, lr, alpha, gamma, max_depth=None, anim=False):
         self.num_iters = num_iters
-        self.num_bins = num_bins
+        # self.num_bins = num_bins
         self.bin_model = DataBinModel(num_bins)
         self.lr = lr
         self.alpha = alpha
@@ -39,7 +39,10 @@ class XGBoostRegressor:
             curr_tree.fit(x, g, h)
             self.trees.append(curr_tree)
             curr_predict = curr_tree.predict(x)
-            y_hat += self.lr * curr_predict
+            if i == 0 or i == self.num_iters - 1:
+                y_hat += curr_predict
+            else:
+                y_hat += self.lr * curr_predict
             if self.anim:
                 animator.add(i, amse(y, y_hat))
             g = y_hat - y
@@ -48,7 +51,10 @@ class XGBoostRegressor:
         x = self.bin_model.transform(x)
         result = np.zeros(x.shape[0])
         for i in range(len(self.trees)):
-            result += self.trees[i].predict(x) * self.lr
+            if i == 0 or i == len(self.trees) - 1:  # 学习率对结果的衰减不作用于第一棵树和最后一棵树
+                result += self.trees[i].predict(x)
+            else:
+                result += self.trees[i].predict(x) * self.lr
         return result
 
     def curr_loss(self, x, y):
@@ -67,7 +73,7 @@ if __name__ == '__main__':
     import sklearn.datasets
     dataset = sklearn.datasets.load_boston()
     data, target = dataset['data'], dataset['target']
-    model = XGBoostRegressor(num_iters=3, num_bins=10, lr=1, alpha=0.01, gamma=0.01, max_depth=10)
+    model = XGBoostRegressor(num_iters=10, num_bins=10, lr=0.3, alpha=0.01, gamma=0.01, max_depth=10)
     model.fit(data, target)
     result = model.transform(data)
     print(amse(result, target))
